a866a7cf96bb1afa8e0547bb5681840eb7af472a
Szymon P <szymonprz123@gmail.com>
2026-08-01 12:28:50 +0200

    github logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="flex flex-row justify-between items-center shadow-xl mb-4 p-4 pr-8">
       <h1 className="text-2xl font-bold">RSA tool</h1>
       <a href="https://github.com/sws6546/rsa_tool">
-        <img src="/public/github.svg" alt="github logo" className="w-10 h-10" />
+        <img src="/github.svg" alt="github logo" className="w-10 h-10" />
       </a>
     </header>
   )
